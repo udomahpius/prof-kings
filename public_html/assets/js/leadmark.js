5e7e50a5ac483dbdaa7259ed6adef6bdb9f3eea9
Pius Udoma <udomahpius5@gmail.com>
2023-05-14 18:08:05 +0100

    init

--- public_html/assets/js/leadmark.js
+++ public_html/assets/js/leadmark.js
@@ -55,25 +55,3 @@ $(window).on("load", function() {
     })
 })
 
-$('.owl-portfolio').owlCarousel({
-    items:4,
-    loop:true,
-    dots: true,
-    nav: true,
-    autoplay: true,
-    margin:30,
-      responsive:{
-          0:{
-              items:1
-          },
-          700:{
-              items:2
-          },
-          1000:{
-              items:3
-          },
-          1600:{
-              items:4
-          }
-      }
-})
